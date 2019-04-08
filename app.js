c46a8a672c46968e1361ee08429d429ec2a2b987
DirtAndGrass <verdant.luke@gmail.com>
2019-04-08 17:52:59 -0400

    hbs view engine, using router

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const app = express();
+
+const usersRouter = require('./routes/users');
+const rootRouter = require('./routes/root');
+
 const port = 9000;
 
-app.get('/', (req, res) => res.send('Hello World!!'));
-app.get('/:text', (req, res) => res.send(req.params.text));
+app.set('view engine', 'hbs');
+// app.set('view options', { layout: 'other' });
+app.use('/inc', express.static('inc'));
 
-app.listen(port, () => console.log(`Sample App ${port}!`));
+app.use('/users', usersRouter);
+app.use('/', rootRouter);
+
+app.get('/hello', (req, res) => res.send('hey there'));
+
+app.get('/numbers/:from-:to', (req, res) => {
+  let a = parseInt(req.params.from);
+  let b = parseInt(req.params.to);
+
+  let html = '';
+
+  if (a > b) {
+    let t = a;
+    a = b;
+    b = t;
+  }
+
+  for (let x = a; x <= b; x++) {
+    html += `<div>${x}</div>`;
+  }
+
+  res.send(html);
+});
+
+app.listen(port, function() {
+  return console.log('Sample App ' + port + '!');
+});
