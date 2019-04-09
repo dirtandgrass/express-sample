f1d5722a42fb022970a5dd0bf14fb5d32e04d91f
DirtAndGrass <verdant.luke@gmail.com>
2019-04-09 15:58:50 -0400

    tuesday update

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const app = express();
 
-const usersRouter = require('./routes/users');
-const rootRouter = require('./routes/root');
+const rootRouter = require("./routes/root");
+const usersRouter = require("./routes/users");
+const numbersRouter = require("./routes/numbers");
 
 const port = 9000;
 
-app.set('view engine', 'hbs');
-// app.set('view options', { layout: 'other' });
-app.use('/inc', express.static('inc'));
+app.set("view engine", "hbs");
 
-app.use('/users', usersRouter);
-app.use('/', rootRouter);
+app.use(cookieParser());
+app.use("/inc", express.static(path.join(__dirname, "inc")));
 
-app.get('/hello', (req, res) => res.send('hey there'));
-
-app.get('/numbers/:from-:to', (req, res) => {
-  let a = parseInt(req.params.from);
-  let b = parseInt(req.params.to);
-
-  let html = '';
-
-  if (a > b) {
-    let t = a;
-    a = b;
-    b = t;
-  }
-
-  for (let x = a; x <= b; x++) {
-    html += `<div>${x}</div>`;
-  }
-
-  res.send(html);
+app.use((req, res, next) => {
+	if (req.cookies.vcount == undefined) {
+		req.vcount = 1;
+		res.cookie("vcount", 1);
+	} else {
+		req.vcount = parseInt(req.cookies.vcount) + 1;
+		res.cookie("vcount", req.vcount);
+	}
+	next();
 });
 
+app.use("/", rootRouter);
+app.use("/users", usersRouter);
+app.use("/numbers", numbersRouter);
+
 app.listen(port, function() {
-  return console.log('Sample App ' + port + '!');
+	return console.log("Sample App " + port + "!");
 });
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/:userId/:email', (req, res) => {
-  let uid = parseInt(req.params.userId);
-  let email = req.params.email;
+//router.use('/settings', userSettingRouter);
+router.get("/:userId/:email", (req, res) => {
+	let uid = parseInt(req.params.userId);
+	let email = req.params.email;
 
-  res.send(`user ${uid}'s email set to ${email}`);
+	res.send(`user ${uid}'s email set to ${email}`);
 });
 
 module.exports = router;
